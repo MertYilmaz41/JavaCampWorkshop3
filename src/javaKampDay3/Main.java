461f302e5c2c0ba931c4faf80e40edad0f16f85b
MertYilmaz41 <mertylmzbusiness@gmail.com>
2021-11-06 21:07:49 +0300

    JavaKamp 3. Gün

--- src/javaKampDay3/Main.java
+++ src/javaKampDay3/Main.java
@@ -4,24 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		Student student1 = new Student(1,"Mert",20);
-		Student student2 = new Student(2,"İlker",19);
 		
 		
 		Instructor instructor1 = new Instructor(1,"Engin",30,"JAVA");
-		Instructor instructor2 = new Instructor(2,"Melih",37,"C#");
-		
+
 		
 		StudentManager studentManager = new StudentManager();
 		studentManager.homework(student1);
-		
 		
 		
 		InstructorManager instructorManager = new InstructorManager();
 		instructorManager.checkHomework(instructor1);
 		
 		
-
-
 	}
 
 }
